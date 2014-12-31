a2197634c7c791d771f84d2b882328c52150198c
HaileyR <hangyeolryu@gmail.com>
2014-12-31 10:18:51 -0800

    show question on dblclick and check answer on console

--- public/js/app.js
+++ public/js/app.js
@@ -10,41 +10,59 @@ $(document).ready(function(){
         var wordElement =
           "<div class='word' value='" + word.id +"' "+"style='top:" + (Math.floor(Math.random() * 80)+5)
           +"%; left:" + (Math.floor(Math.random() * 80)+5)
-          +"%; font-size: "+((Math.random() * 2)+0.8)+"em'>" + word.spelling
-          +"<br><p class=\'meaning\' id=\'w"+ word.id +"\' style=\'display: none\'>"+word.meaning+"</p></div>"
+          +"%; font-size: "+((Math.random() * 2)+0.8)+"em'><p class=\'spelling\'>" + word.spelling + "</p>"
+          +"<p class=\'meaning\' id=\'w"+ word.id +"\' style=\'display: none\'>"+word.meaning+"</p></div>"
         $('#container').append(wordElement);
       })
 
     });
   });
 
-  $('#container').on('dblclick', '.word', function(event){
+  $('#container').on('dblclick', '.spelling', function(event){
     $target = $(event.target);
-    $('#w'+$target.attr('value')).replaceWith('<div class=\'answers\'></div>');
-    randomAnswers(meanings).forEach(function(answer){
-      var answerElement =
-        '<ul><input type=\'radio\' name=\'response'+ ($target.attr('value')) +'\'>' + answer + '</ul>'
-      $('.answers').append(answerElement);
+    var answer = $target.siblings().text();
+    var question = randomChoices(meanings, answer)
+    $('#w'+$target.closest('.word').attr('value')).replaceWith('<form id=\'question\'></form>');
+    question[0].forEach(function(choice, index){
+      var choiceElement =
+        '<ul><input type=\'radio\' name=\'response\' value=\''+index+'\'>' + choice + '</ul>'
+      $('#question').append(choiceElement);
 
     });
-    $('.answers').append('<button>Submit</button>');
-    $("#container").children().bind('click dblclick', function(){ return false; });
+    $('#question').append('<button type="submit" form="question" id=\'ansCheck\'>Submit</button>');
+    $(".spelling").bind('dblclick', function(){ return false; });
+    $(".spelling").bind('click', function(){ return false; });
+
+    $('#question').on('submit', function(event){
+      event.preventDefault();
+      $target = $(event.target);
+      if(question[1] === parseInt($('input[name="response"]:checked').val())){
+        console.log('correct');
+      }else{
+        console.log('wrong');
+      };
+      // $(".spelling").unbind('click');
+    });
+
+
 
   });
 
-  $('#container').on('click', '.word', function(event){
+  $('#container').on('click', '.spelling', function(event){
     setTimeout(function(){
       $target = $(event.target);
-      $("#container").children().bind('click', function(){ return false; });
-      $('#w'+$target.attr('value')).css('display', 'inline-block');
+      $(".spelling").bind('dblclick', function(){ return false; });
+    $(".spelling").bind('click', function(){ return false; });
+      $('#w'+$target.closest('.word').attr('value')).css('display', 'inline-block');
+      setTimeout(function(){
+        $('#w'+$target.closest('.word').attr('value')).css('display', 'none');
+        $(".spelling").unbind('click');
+        $(".spelling").unbind('dblclick');
+      }, 2500);
     }, 500);
 
-
-    setTimeout(function(){
-      $('#w'+$target.attr('value')).css('display', 'none');
-      $("#container").children().unbind('click');
-    }, 2500);
   });
+
 
 
 
@@ -52,11 +70,13 @@ $(document).ready(function(){
 
 });
 
-function randomAnswers(array){
+function randomChoices(array, answer){
   a = []
-  for(i=0; i<4; i++){
+  for(i=0; i<3; i++){
     rand = Math.floor(Math.random() * array.length)
     a.push(meanings[rand]);
   };
-  return a;
+
+  a.splice(Math.floor(Math.random() * a.length), 0, answer);
+  return [a, a.indexOf(answer)];
 };
