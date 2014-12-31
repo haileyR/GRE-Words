35f281da55c04e87a713e1065e9217d73809ce9e
HaileyR <hangyeolryu@gmail.com>
2014-12-31 14:05:45 -0800

    answer submit button click will replace question with hidden meaning p

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
       url: '/'
     }).done(function(response){
       meanings = response[1];
-      console.log(response);
       response[0].forEach(function(word){
         var wordElement =
           "<div class='word' value='" + word.id +"' "+"style='top:" + (Math.floor(Math.random() * 80)+5)
@@ -19,47 +18,57 @@ $(document).ready(function(){
   });
 
   $('#container').on('dblclick', '.spelling', function(event){
-    $target = $(event.target);
-    var answer = $target.siblings().text();
-    var question = randomChoices(meanings, answer)
-    $('#w'+$target.closest('.word').attr('value')).replaceWith('<form id=\'question\'></form>');
-    question[0].forEach(function(choice, index){
-      var choiceElement =
-        '<ul><input type=\'radio\' name=\'response\' value=\''+index+'\'>' + choice + '</ul>'
-      $('#question').append(choiceElement);
-
-    });
-    $('#question').append('<button type="submit" form="question" id=\'ansCheck\'>Submit</button>');
-    $(".spelling").bind('dblclick', function(){ return false; });
-    $(".spelling").bind('click', function(){ return false; });
-
-    $('#question').on('submit', function(event){
-      event.preventDefault();
+    if(!(($('#question').is(':visible')) || ($('.meaning').is(':visible')))){
       $target = $(event.target);
-      if(question[1] === parseInt($('input[name="response"]:checked').val())){
-        console.log('correct');
-      }else{
-        console.log('wrong');
-      };
-      // $(".spelling").unbind('click');
-    });
+      console.log($target.siblings());
+      var meaningP = $target.siblings();
+      var answer = $target.siblings().text();
+      var question = randomChoices(meanings, answer)
+      $('#w'+$target.closest('.word').attr('value')).replaceWith('<form id=\'question\'></form>');
+      question[0].forEach(function(choice, index){
+        var choiceElement =
+          '<ul><input type=\'radio\' name=\'response\' value=\''+index+'\'>' + choice + '</ul>'
+        $('#question').append(choiceElement);
 
+      });
+      $('#question').append('<button type="submit" form="question" id=\'ansCheck\'>Submit</button>');
+      $(".spelling").bind('dblclick', function(){ return false; });
+      $(".spelling").bind('click', function(){ return false; });
 
+      $('#question').on('submit', function(event){
+        event.preventDefault();
+        $target = $(event.target);
+        console.log($target.siblings());
+        console.log(meaningP);
+        if(question[1] === parseInt($('input[name="response"]:checked').val())){
+          console.log('correct');
+        }else{
+          console.log('wrong');
+        };
+        $($target).replaceWith(meaningP);
+        $(".spelling").unbind('click');
+        $(".spelling").unbind('dblclick');
+      });
+
+    };
 
   });
 
   $('#container').on('click', '.spelling', function(event){
     setTimeout(function(){
-      $target = $(event.target);
-      $(".spelling").bind('dblclick', function(){ return false; });
-    $(".spelling").bind('click', function(){ return false; });
-      $('#w'+$target.closest('.word').attr('value')).css('display', 'inline-block');
-      setTimeout(function(){
-        $('#w'+$target.closest('.word').attr('value')).css('display', 'none');
-        $(".spelling").unbind('click');
-        $(".spelling").unbind('dblclick');
-      }, 2500);
-    }, 500);
+      if(!(($('#question').is(':visible')) || ($('.meaning').is(':visible')))){
+        $target = $(event.target);
+        $(".spelling").bind('dblclick', function(){ return false; });
+        $(".spelling").bind('click', function(){ return false; });
+        $('#w'+$target.closest('.word').attr('value')).css('display', 'inline-block');
+        setTimeout(function(){
+          // debugger
+          $('#w'+$target.closest('.word').attr('value')).css('display', 'none');
+          $(".spelling").unbind('click');
+          $(".spelling").unbind('dblclick');
+        }, 2500);
+      }
+    }, 300);
 
   });
 
